0988a47c3db21efaef494fd1782103f4f2cd40d2
panatale1 <panatale1@gmail.com>
2013-09-12 17:43:47 -0400

    End of Class 9/12/13
    Delegates, Lambdas, Extensions

---
+++ ConsoleApplication1/ConsoleApplication1/Vehicles.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Vehicle
+    {
+        public Vehicle(){
+            BreakDown = x => Log += "\nWe Broke Down";
+        }
+        public const int Speed = 10;
+        public IEnumerable<string> Seats = new List<string> {"Driver","Shotgun","Rear", "Backseat Driver" };
+        public String Log { get; set; }
+        public virtual void Drive()
+        {
+            Log += "\nWe Drove Somewhere";
+        }
+        public Action<string> BreakDown { get; set; }
+        public void Crash(Func<int, int> impact){
+            foreach (var seat in Seats)
+            {
+                Log += "\n" + seat + " affected";
+            }
+            Log += "\nthat is: " + string.Join(", ", Seats);
+            Log += "\r\nThe impact was: " + impact(Speed);
+        }
+    }
+
+    public class Car:Vehicle
+    {
+        public void Park()
+        {
+            Drive();
+            // get into parked state
+        }
+    }
+    public class Plane : Vehicle
+    {
+        public Plane()
+            : base()
+        {
+            BreakDown = (x) => Log += "\nWe Fell Down";
+        }
+        //Can only override VIRTUAL functions
+        public override void Drive()
+        {
+            Log += "\nWe Flew Somewhere";
+        }
+    }
+
+    public static class Extensions
+    {
+        public static void Transform(this Vehicle v)
+        {
+            v.Log += "\nI am now an Autobot";
+        }
+    }
+    //LINQ kinda helps C# do functional programming
+}
---
+++ ConsoleApplication1/UnitTestProject1/VehicleUnitTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class VehicleUnitTest
+    {
+        [TestMethod]
+        public void VehicleCanDrive()
+        {
+            var v = new Vehicle();
+            v.Drive();
+            Assert.IsTrue(v.Log.Contains("Drove"));
+            v.BreakDown("OUt of Gas");
+            Assert.IsTrue(v.Log.Contains("Broke"));
+        }
+        [TestMethod]
+        public void PlaneCanFly()
+        {
+            var v = new Plane();
+            v.Drive();
+            Assert.IsTrue(v.Log.Contains("Flew"));
+            v.BreakDown("Engine Failure");
+            Assert.IsTrue(v.Log.Contains("Fell"));
+            v.BreakDown = x => v.Log += "\nWe Are Just Fine" + x;
+            v.Log = "";
+            v.BreakDown("Gremlins");
+            Assert.IsTrue(v.Log.Contains("Fine"));
+        }
+        [TestMethod]
+        public void CrashesHurt()
+        {
+            var v = new Vehicle();
+            v.Crash(x => x);
+            Assert.IsTrue(v.Log.Contains("1"));
+            v.Crash(x => x * 55);
+            Assert.IsTrue(v.Log.Contains("55"));
+        }
+        [TestMethod]
+        public void VehiclesCanTransform()
+        {
+            var v = new Vehicle();
+            v.Transform();
+            Assert.IsTrue(v.Log.Contains("Autobot"));
+        }
+    }
+}
