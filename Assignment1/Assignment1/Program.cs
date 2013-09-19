906fa9ed23a4eeb8127868486e60c5d4c914f891
panatale1 <panatale1@gmail.com>
2013-09-19 16:18:02 -0400

    Assignment1 Due 9/19/13

---
+++ Assignment1/Assignment1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+
+    public class Assignment1
+    {
+
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+    public class Person
+    {
+        public String Name { get; set; }
+        public Person()
+        {
+
+        }
+    }
+
+}
---
+++ Assignment1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using Assignment1;
+using System.Collections.Generic;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        IEnumerable<string> names = new[]{"Bessie","Vashti","Frederica","Nisha","Kendall","Magdalena","Brendon","Eve","Manda","Elvera","Miquel","Tyra","Lucie","Marvella","Tracee","Ramiro","Irene","Davina","Jeromy","Siu"};
+
+        [TestMethod]
+        public void MakeListOfPeople()
+        {
+            var actual = names.Select(x => new Person { Name = x });
+        }
+
+        [TestMethod]
+        public void NamesThatStartWithM()
+        {
+            var actual = names.Select(x => new Person { Name = x }).Where(x => x.Name.StartsWith("M"));
+        }
+        [TestMethod]
+        public void NamesInUpperCase()
+        {
+            //I assumed that by "same list," you meant the same list as the initial list
+            var actual = names.Select(x => new Person { Name = x.ToUpper() });
+        }
+        [TestMethod]
+        public void LengthOfName()
+        {
+            var actual = names.Select(x => x.Length).ToArray();
+        }
+        [TestMethod]
+        public void FirstThreeLettersOrderedByname()
+        {
+            var actual = names.Select(x => x.Substring(0,3)).OrderBy(x => x);
+        }
+    }
+}
