d5aab0d4f798f4b3a8c7965e995102bf0bdba711
panatale1 <panatale1@gmail.com>
2013-11-25 17:48:28 -0500

    classwork with MVC5

---
+++ Class From 11-18-13/2013FallCSharp-master/WebMVC/Controllers/ContactsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DataLayer.Models;
+
+namespace WebMVC.Controllers
+{
+    [Authorize]
+    public class ContactsController : Controller
+    {
+        private CSharpContext db = new CSharpContext();
+
+        // GET: /Contacts/
+        public ActionResult Index()
+        {
+            var contacts = db.Contacts.Include(c => c.Keyword);
+            return View(contacts.ToList());
+        }
+
+        // GET: /Contacts/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contact contact = db.Contacts.Find(id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contact);
+        }
+
+        // GET: /Contacts/Create
+        public ActionResult Create()
+        {
+            ViewBag.KeywordId = new SelectList(db.Keywords, "Id", "Name");
+            return View();
+        }
+
+        // POST: /Contacts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="Id,FirstName,LastName,KeywordId,fbid")] Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Contacts.Add(contact);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.KeywordId = new SelectList(db.Keywords, "Id", "Name", contact.KeywordId);
+            return View(contact);
+        }
+
+        // GET: /Contacts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contact contact = db.Contacts.Find(id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.KeywordId = new SelectList(db.Keywords, "Id", "Name", contact.KeywordId);
+            return View(contact);
+        }
+
+        // POST: /Contacts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="Id,FirstName,LastName,KeywordId,fbid")] Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(contact).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.KeywordId = new SelectList(db.Keywords, "Id", "Name", contact.KeywordId);
+            return View(contact);
+        }
+
+        // GET: /Contacts/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contact contact = db.Contacts.Find(id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contact);
+        }
+
+        // POST: /Contacts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Contact contact = db.Contacts.Find(id);
+            db.Contacts.Remove(contact);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
